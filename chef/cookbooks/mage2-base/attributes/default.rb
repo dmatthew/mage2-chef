c3d8b6a0de8e5b6c5f4985c2fdd4de0e8b1138ab
Matt Davis <madavis111@outlook.com>
2016-08-28 18:44:59 -0700

    Move base attributes to default attributes file.

---
+++ chef/cookbooks/mage2-base/attributes/default.rb
@@ -0,0 +1,24 @@
+default['mage2']['packages'] = [
+    'build-essential',
+    'curl',
+    'git',
+    'gcc',
+    'gnupg',
+    'htop',
+    'lsof',
+    'lvm2',
+    'make',
+    'ntp',
+    'openssh-server',
+    'openssl',
+    'pv',
+    'ssmtp',
+    'strace',
+    'tmux',
+    'unzip',
+    'vim',
+]
+default['mage2']['uninstall'] = [
+    'apparmor',
+    'puppet',
+]
--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -5,30 +5,4 @@ run_list(
 )
 
 default_attributes(
-    'mage2' => {
-        'packages' => [
-            'build-essential',
-            'curl',
-            'git',
-            'gcc',
-            'gnupg',
-            'htop',
-            'lsof',
-            'lvm2',
-            'make',
-            'ntp',
-            'openssh-server',
-            'openssl',
-            'pv',
-            'ssmtp',
-            'strace',
-            'tmux',
-            'unzip',
-            'vim',
-        ],
-        'uninstall' => [
-            'apparmor',
-            'puppet',
-        ]
-    },
 )
